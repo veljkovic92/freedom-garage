515774b0cae87c973f5ca78a249d6192633f8e32
Stefan Veljkovic <stefanveljkovic@yahoo.com>
2022-10-17 11:12:10 +0200

    installed react-icons, edited photo and photo edit button, edited change account information section and added arrows

--- src/pages/MyAccount.js
+++ src/pages/MyAccount.js
@@ -1,9 +1,20 @@
 import { useSelector } from "react-redux";
 import classes from "./MyAccount.module.css";
 import profileImage from "../assets/profile-template.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaArrowDown } from "react-icons/fa";
 
 const MyAccount = () => {
   const user = useSelector((state) => state.auth.user);
+
+  const onNameChangeHandler = () => {
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDUlyw-dltmGCc-EjQuuVbGAQpX92HFL0I",
+      {}
+    );
+  };
+
+  const onPasswordChangeHandler = () => {};
 
   return (
     <div className={classes["my-account"]}>
@@ -13,15 +24,25 @@ const MyAccount = () => {
           <span>My Name:</span>
           <span>My E-Mail: {user}</span>
         </div>
-        <div>
-          <img alt="My Photo" src={profileImage}/>
+        <div className={classes["account-info__right"]}>
+          <img alt="My Photo" src={profileImage} />
+          <a>Edit</a>
         </div>
       </div>
       <h3>Change My Account Information</h3>
       <div className={classes["account-edit"]}>
-        <span>Change my password</span>
-        <span>Change my photo</span>
-        <span>Delete my account</span>
+        <div className={classes["name-edit"]}>
+          <a onClick={onNameChangeHandler}>Change my name</a>
+          <FaArrowDown />
+        </div>
+        <div className={classes["password-edit"]}>
+          <a onClick={onPasswordChangeHandler}>Change my password</a>
+          <FaArrowDown />
+        </div>
+        <div>
+          <a>Delete my account</a>
+          <FaArrowDown />
+        </div>
       </div>
     </div>
   );
