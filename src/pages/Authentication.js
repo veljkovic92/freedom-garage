b8a52f73570b0c381f446e7203028e6a53c4c14e
Stefan Veljkovic <stefanveljkovic@yahoo.com>
2022-11-13 20:39:39 +0100

    <768px + <375px v3

--- src/pages/Authentication.js
+++ src/pages/Authentication.js
@@ -1,7 +1,12 @@
+import classes from "./Authentication.module.css";
 import AuthForm from "../components/AuthForm/AuthForm";
 
 const Authentication = () => {
-  return <AuthForm />;
+  return (
+    <div className={classes.auth}>
+      <AuthForm />
+    </div>
+  );
 };
 
 export default Authentication;
